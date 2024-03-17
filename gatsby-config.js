1449bd1eebbc288ca0dd6d822416c48a1e14f0cb
Phibzzz <philippe.haag@com-ingenious.fr>
2024-03-17 19:23:41 +0100

    MAJ meta et gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Philippe Haag',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Expert polyvalent en technologies de pointe, je suis un développeur passionné, un data scientist chevronné, un chercheur en intelligence artificielle et un consultant en stratégie de communication.',
+    siteUrl: 'https://phibzzz.dev', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Philippe Haag',
+        short_name: 'Philippe Haag',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
