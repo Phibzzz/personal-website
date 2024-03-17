42457a287a8cdae4c3c2ed89f2cd51e8f8a06b65
Phibzzz <philippe.haag@com-ingenious.fr>
2024-03-17 21:37:32 +0100

    MAJ icons, favicons, meta

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
   }, []);
 
   const one = <h1>#DataScientist #IA #Formateur #Consultant #DevWeb</h1>;
-  const two = <h2 className="big-heading">Philippe HAAG.</h2>;
+  const two = <h2 className="big-heading">Philippe Haag.</h2>;
   const three = <h3 className="big-heading">Techweb, IA & Communication.</h3>;
   const four = (
     <>
